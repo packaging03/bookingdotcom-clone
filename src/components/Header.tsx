2ae2e2197efbdee841045c4f8911d8b604e171db
Adeyemi Olaoye <yhemi06@gmail.com>
2023-12-17 04:33:45 +0100

    I added menu icon for mobile screen

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,38 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
 
+import { Bars3Icon } from "@heroicons/react/20/solid";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 
 function Header() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
   return (
     <header className="bg-[#013B94]">
       <nav
         className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
         aria-label="Global"
       >
-        <Link href="/" className="-m-1.5 p-1.5">
-          <span className="sr-only">Booking.com</span>
-          <img
-            className="h-12 w-auto"
-            src="https://static1.squarespace.com/static/5bde0f00c3c16aa95581e2e2/62b4cb1add9d257dd43bb03d/62b653fedc7c895918d19b24/1656116254983/booking+logo+white.png?format=1500w"
-            alt=""
-          />
-        </Link>
+        <div className="flex lg:flex-1">
+          <Link href="/" className="-m-1.5 p-1.5">
+            <span className="sr-only">Booking.com</span>
+            <img
+              className="h-12 w-auto"
+              src="https://static1.squarespace.com/static/5bde0f00c3c16aa95581e2e2/62b4cb1add9d257dd43bb03d/62b653fedc7c895918d19b24/1656116254983/booking+logo+white.png?format=1500w"
+              alt=""
+            />
+          </Link>
+        </div>
+        <div className="flex lg:hidden">
+          <button
+            type="button"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-white"
+            onClick={() => setMobileMenuOpen(true)}
+          >
+            <span className="sr-only">Open main menu</span>
+            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+          </button>
+        </div>
       </nav>
     </header>
   );
