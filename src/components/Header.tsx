1b4f90b41f7c7c885e9d1629df17475051c4c4ed
Adeyemi Olaoye <yhemi06@gmail.com>
2023-12-24 04:20:33 +0100

    more changes on Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,39 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
 
-import { Bars3Icon } from "@heroicons/react/20/solid";
+import { Popover, Transition, Dialog, Disclosure } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import {
+  ChatBubbleLeftIcon,
+  HomeIcon,
+  ChevronDownIcon,
+  PhoneIcon,
+  PlayCircleIcon,
+  PaperAirplaneIcon,
+} from "@heroicons/react/20/solid";
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { Fragment, useState } from "react";
+
+const products = [
+  {
+    name: "Book a Stay",
+    description: "Get a better understanding of your traffic",
+    href: "#",
+    icon: HomeIcon,
+  },
+  {
+    name: "Book a Flight",
+    description: "Speak directly to your customers",
+    href: "#",
+    icon: PaperAirplaneIcon,
+  },
+  {
+    name: "Contact our Support Team",
+    description: "Your customers' data will be safe and secure",
+    href: "#",
+    icon: ChatBubbleLeftIcon,
+  },
+];
 
 function Header() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
@@ -33,6 +63,58 @@ function Header() {
             <Bars3Icon className="h-6 w-6" aria-hidden="true" />
           </button>
         </div>
+
+        <Popover.Group className="hidden lg:flex lg:gap-x-12">
+          <Popover className="relative">
+            <Popover.Button className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-white">
+              Stays
+              <ChevronDownIcon
+                className="h-5 w-5 flex-none text-white"
+                aria-hidden="true"
+              />
+            </Popover.Button>
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-200"
+              enterFrom="opacity-0 translate-y-1"
+              enterTo="opacity-100 translate-y-0"
+              leave="transition ease-in duration-150"
+              leaveFrom="opacity-100 translate-y-0"
+              leaveTo="opacity-0 translate-y-1"
+            >
+              <Popover.Panel className="absolute bg-white -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl shadow-lg ring-1 ring-gray-900/5">
+                <div className="p-4">
+                  {products.map((item) => (
+                    <div
+                      key={item.name}
+                      className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50"
+                    >
+                      <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-gray-200">
+                        <item.icon
+                          className="h-6 w-6 text-[#013B94] group-hover:text-blue-600"
+                          aria-hidden="true"
+                        />
+                      </div>
+                      <div className="flex-auto">
+                        <a
+                          href={item.href}
+                          className="block font-semibold text-[#013B94]"
+                        >
+                          {item.name}
+                          <span className="absolute inset-0" />
+                        </a>
+                        <p className="mt-1 text-[#013B94]">
+                          {item.description}
+                        </p>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+                
+              </Popover.Panel>
+            </Transition>
+          </Popover>
+        </Popover.Group>
       </nav>
     </header>
   );
