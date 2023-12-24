426d5fc6deb64ec85227dfe24d977d7c94e08eae
Adeyemi Olaoye <yhemi06@gmail.com>
2023-12-24 22:33:39 +0100

    adding  hook form library and more content to the home page

---
+++ src/components/SearchForm.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import React from "react";
+import * as z from "zod";
+
+export const formSchema = z.object({
+  location: z.string().min(2).max(50),
+  dates: z.object({
+    from: z.date(),
+    to: z.date(),
+  }),
+  adults: z
+    .string()
+    .min(1, {
+      message: "Please select at least 1 adult",
+    })
+    .max(12, { message: "Max 12 adults Occupancy" }),
+  children: z.string().min(0).max(12, { message: "Max 12 children Occupacy" }),
+  rooms: z.string().min(1, { message: "Please select at least 1 room" }),
+});
+
+function SearchForm() {
+  return <div></div>;
+}
+
+export default SearchForm;
