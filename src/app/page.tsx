c0d03d1cbdb7011e5e5afd895ae881b84b3c13d0
Adeyemi Olaoye <yhemi06@gmail.com>
2023-12-25 06:04:11 +0100

    made further changes to search form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,19 @@
 /* eslint-disable @next/next/no-img-element */
+import SearchForm from "@/components/SearchForm";
 import { trending_data } from "../../data/trending";
 
 export default function Home() {
   return (
     <main className="bg-[#013B94]">
       <section className="max-w-7xl mx-auto p-6">
-        <h2 className="font-bold text-5xl text-white">Find your ext Stay</h2>
+        <h2 className="font-bold text-5xl text-white">Find your Next Stay</h2>
         <h3 className="text-white py-5 text-xl">
           Search low prices on hotels, homes and much more...
         </h3>
       </section>
-      <section className="m-4 mt-0 -mb-14 px-2 lg:px-4"></section>
+      <section className="m-4 mt-0 -mb-14 px-2 lg:px-4">
+        <SearchForm />
+      </section>
 
       <section className="mx-auto max-w-7xl mt-10 p-6 bg-white rounded-t-lg">
         <div className="pt-5">
