f28af4ddda7203b3d7ebed3c1aec50c0e2a13a76
Adeyemi Olaoye <yhemi06@gmail.com>
2023-12-25 06:57:17 +0100

    added search page

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,13 +1,14 @@
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useRouter } from "next/router";
-import React from "react";
+import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 import { Button } from "@/components/ui/button";
+import { BedDoubleIcon, CalendarIcon } from "lucide-react";
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
 import { format } from "date-fns";
-import { cn } from "@/lib/utils";
+
 import {
   Form,
   FormControl,
@@ -15,10 +16,9 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "./ui/form";
-import { Input } from "./ui/input";
-import { BedDoubleIcon, CalendarIcon } from "lucide-react";
-import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { cn } from "@/lib/utils";
 import { Calendar } from "./ui/calendar";
 
 export const formSchema = z.object({
@@ -33,12 +33,17 @@ export const formSchema = z.object({
       message: "Please select at least 1 adult",
     })
     .max(12, { message: "Max 12 adults Occupancy" }),
-  children: z.string().min(0).max(12, { message: "Max 12 children Occupacy" }),
-  rooms: z.string().min(1, { message: "Please select at least 1 room" }),
+  children: z.string().min(0).max(12, {
+    message: "Max 12 children Occupancy",
+  }),
+  rooms: z.string().min(1, {
+    message: "Please select at least 1 room",
+  }),
 });
 
 function SearchForm() {
-  //   const router = useRouter();
+  const router = useRouter();
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -52,7 +57,31 @@ function SearchForm() {
       rooms: "1",
     },
   });
-  function onSubmit(values: z.infer<typeof formSchema>) {}
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+
+    const checkin_monthday = values.dates.from.getDate().toString();
+    const checkin_month = (values.dates.from.getMonth() + 1).toString();
+    const checkin_year = values.dates.from.getFullYear().toString();
+    const checkout_monthday = values.dates.to.getDate().toString();
+    const checkout_month = (values.dates.to.getMonth() + 1).toString();
+    const checkout_year = values.dates.to.getFullYear().toString();
+
+    const checkin = `${checkin_year}-${checkin_month}-${checkin_monthday}`;
+    const checkout = `${checkout_year}-${checkout_month}-${checkout_monthday}`;
+
+    const url = new URL("https://www.booking.com/searchresults.html");
+    url.searchParams.set("ss", values.location);
+    url.searchParams.set("group_adults", values.adults);
+    url.searchParams.set("group_children", values.children);
+    url.searchParams.set("no_rooms", values.rooms);
+    url.searchParams.set("checkin", checkin);
+    url.searchParams.set("checkout", checkout);
+
+    router.push(`/search?url=${url.href}`);
+  }
+
   return (
     <Form {...form}>
       <form
@@ -70,15 +99,17 @@ function SearchForm() {
                   <BedDoubleIcon className="ml-2 h-4 w-4 text-white" />
                 </FormLabel>
 
+                <FormMessage />
+
                 <FormControl>
                   <Input placeholder="London, UK" {...field} />
                 </FormControl>
-                <FormMessage />
               </FormItem>
             )}
           />
         </div>
-        <div className="grid w-full flex-1 lg:max-w-sm items-center gap-1.5">
+
+        <div className="grid w-full lg:max-w-sm flex-1 items-center gap-1.5">
           <FormField
             control={form.control}
             name="dates"
@@ -86,6 +117,7 @@ function SearchForm() {
               <FormItem className="flex flex-col">
                 <FormLabel className="text-white">Dates</FormLabel>
                 <FormMessage />
+
                 <Popover>
                   <PopoverTrigger asChild>
                     <FormControl>
@@ -132,6 +164,7 @@ function SearchForm() {
             )}
           />
         </div>
+
         <div className="flex w-full items-center space-x-2">
           <div className="grid items-center flex-1">
             <FormField
@@ -148,6 +181,7 @@ function SearchForm() {
               )}
             />
           </div>
+
           <div className="grid items-center flex-1">
             <FormField
               control={form.control}
@@ -163,6 +197,7 @@ function SearchForm() {
               )}
             />
           </div>
+
           <div className="grid items-center flex-1">
             <FormField
               control={form.control}
@@ -172,12 +207,13 @@ function SearchForm() {
                   <FormLabel className="text-white">Rooms</FormLabel>
                   <FormMessage />
                   <FormControl>
-                    <Input type="number" placeholder="Rooms" {...field} />
+                    <Input type="number" placeholder="rooms" {...field} />
                   </FormControl>
                 </FormItem>
               )}
             />
           </div>
+
           <div className="mt-auto">
             <Button type="submit" className="bg-blue-500 text-base">
               Search
