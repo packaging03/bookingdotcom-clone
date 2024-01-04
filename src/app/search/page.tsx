e9f8bba482b12b780dee108b608ba0b4f637e927
Adeyemi Olaoye <yhemi06@gmail.com>
2024-01-04 02:45:31 +0100

    validity period expired

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -20,7 +20,7 @@ async function SearchPage({ searchParams }: Props) {
 
   const results = await fetchResults(searchParams);
 
-  if (!results) return <div>No results...</div>;
+  if (!results) return <div>Sorry, your API trial period has expired</div>;
 
   return (
     <section>
